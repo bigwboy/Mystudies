4d4ce7de5108f307a9c67bb8095587e39c629c69
光光 <kevinliu830829@163.com>
2017-11-09 15:10:47 +0800

    添加sigmod回归，

---
+++ Stochastic.py
@@ -0,0 +1,152 @@
+# -*- coding: utf8 -*-
+#time:2017/11/9 下午2:59
+#VERSION:1.0
+#__OUTHOR__:guangguang
+
+import numpy as np
+from random import seed
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from matplotlib.colors import ListedColormap
+
+
+
+class AdalineSGD(object):
+    """ADAptive LInear NEuron classifier.
+    Parameters
+    ---------------
+    eta: float
+        Learning rate (between 0.0 and 1.0)
+    n_iter: int
+        Passes over the training dataset.
+    Attributes
+    ---------------
+    w_: 1d-array
+        Weights after fitting.
+    errors_: list
+        Number of misclassification in every epoch.
+    shuffle: bool (default: True)
+        Shuffles training data every epoch
+        if True to prevent cycles.
+    random_state: int (default: None)
+        Set random state for shuffling
+        and initializing the weights.
+"""
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+
+    def fit(self, X, y):
+        """Fit training data.
+    Parameters
+    -----------
+    X: {array-like}, shape=[n_samples, n_features]
+    y: array-like, shape=[n_samples]
+    Returns
+    --------------
+    self: object
+    """
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+                avg_cost = sum(cost) / len(y)
+                self.cost_.append(avg_cost)
+        return self
+
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights."""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+
+    def _initialize_weights(self, m):
+        """Initialize weights to zeros"""
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.net_input(xi)
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
+
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return self.net_input(X)
+
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+
+
+#分类显示
+def plot_decision_region(X, y, classifier, resolution=0.02):
+    makers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contour(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=makers[idx], label=cl)
+
+
+
+#debug
+if __name__ == "__main__":
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+    # print  df.tail()
+    # 获取数据
+    y = df.iloc[0:100, 4].values
+    # 转换
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
+    # 数据显示
+    plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1], color='b', marker='x', label='versicolor')
+    plt.xlabel('petal length')
+    plt.ylabel('sepal length')
+    plt.legend(loc='upper left')
+    plt.show()
