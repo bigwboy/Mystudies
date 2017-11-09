8869c54ef083dbe1262be241a496503502965372
光光 <kevinliu830829@163.com>
2017-11-09 16:46:37 +0800

    添加sigmod回归，

--- Stochastic.py
+++ Stochastic.py
@@ -143,10 +143,28 @@ if __name__ == "__main__":
     # 转换
     y = np.where(y == 'Iris-setosa', -1, 1)
     X = df.iloc[0:100, [0, 2]].values
+    X_std = np.copy(X)
+    X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+    X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+
     # 数据显示
     plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
     plt.scatter(X[50:100, 0], X[50:100, 1], color='b', marker='x', label='versicolor')
     plt.xlabel('petal length')
     plt.ylabel('sepal length')
     plt.legend(loc='upper left')
+    plt.show()
+
+    ada = AdalineSGD(n_iter=10, eta=0.01,random_state=1 )
+    ada.fit(X_std,y)
+    plot_decision_region(X_std,y,classifier=ada)
+    plt.title('随机梯度下降')
+    plt.xlabel('sepal length')
+    plt.ylabel('petal length')
+    plt.legend(loc='upper left')
+    plt.show()
+    plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('average cost')
     plt.show()
